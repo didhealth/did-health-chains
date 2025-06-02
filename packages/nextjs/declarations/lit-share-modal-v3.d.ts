1e097d73d01546e3e3e1fe153530568693efaff7
metacare <metacare@dcl.health>
2025-06-02 19:29:25 -0400

    cnange project structure - remove dapps

--- packages/nextjs/declarations/lit-share-modal-v3.d.ts
+++
@@ -1,8 +0,0 @@
-// in a file like types/lit-share-modal-v3.d.ts
-declare module 'lit-share-modal-v3' {
-    var ShareModal: React.ComponentType<{
-      onClose: () => void;
-      onUnifiedAccessControlConditionsSelected: (shareModalOutput: any) => void;
-    }>;
-    export = ShareModal;
-  }
